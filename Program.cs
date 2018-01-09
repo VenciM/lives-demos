55ae943b9a5da2890f46785f2fbb34215daa3e2e
VenciM <ventsislav83@gmail.com>
2018-01-09 10:03:52 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace passedOrGrade
+{
+   public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            double num = double.Parse(Console.ReadLine());
+
+            if (num>2 & num<=6)
+            {
+                Console.WriteLine("{0} passed with: ", num);
+                   
+            }
+            else
+            {
+                Console.WriteLine("grade");
+            }
+        }
+    }
+}
